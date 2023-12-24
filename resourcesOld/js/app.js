da7632551be19ffead995c109b0398e59cd96f25
mercurystudio99 <mercurystudio99@gmail.com>
2023-12-24 00:50:35 -0800

    delete old files

--- resourcesOld/js/app.js
+++
@@ -1,4 +0,0 @@
-require('./bootstrap');
-require('./parts/ajax_setup');
-require('./parts/captcha');
-require('./parts/make-summernote');
--- resourcesOld/js/parts/products_lists.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    "use strict";
-
-    $('body').on('change', '#topFilters input,#topFilters select', function (e) {
-        e.preventDefault();
-        $('#filtersForm').trigger('submit');
-    });
-
-})(jQuery);
